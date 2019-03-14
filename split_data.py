9c4d7bca03d42ee4c56e02ef39f87bb8ddee5a61
Sandra Derbring <sandra.derbring@gmail.com>
2019-03-14 21:58:23 +0100

    removed obsolete code

--- split_data.py
+++ split_data.py
@@ -6,23 +6,27 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 
 
-def split_data(target, source, size):
+def split_data(size):
+
+    #Getting data from readfile.py
+    target, source = readfile("UN-english.txt", "UN-french.txt")
+
+    #Printing the lengths of target and source data
+    print("Total length of target language: ".format(len(t)))
+    print("Total length of source language: ".format(len(s)))
     
     #Splitting data into sets
     print("Splitting data into training and tests sets...")
     targetTrain, targetTest, sourceTrain, sourceTest = train_test_split(target, source, test_size=size)
 
+    #Printing the lengths of the sets
     print("Length of target training set: ".format(len(targetTrain)))
     print("Length of target test set: ".format(len(targetTest)))
     print("Length of source training set: ".format(len(sourceTrain)))
     print("Length of source test set: ".format(len(sourceTest)))
 
+    return targetTrain, targetTest, sourceTrain, sourceTest
 
-#Getting data from readfile.py
-target, source = readfile("UN-english.txt", "UN-french.txt")
-
-print("Total length of target language: ".format(len(t)))
-print("Total length of source language: ".format(len(s)))
 
 #Specifying test size in percentage
 test_size = 0.2
