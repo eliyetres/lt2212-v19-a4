4f58eff32f43dd17880c2afa363f66cb7b6d8679
eliyetres <eliyetres@hotmail.com>
2019-03-15 11:14:14 +0100

    loaded google model in model.py and updated config.py with google vector variable

--- config.py
+++ config.py
@@ -1,2 +1,8 @@
-english_filename = "../UN-english-sample.txt"
-french_filename = "../UN-french-sample.txt"
+english_filename = "UN-english.txt"
+french_filename = "UN-french.txt"
+
+#english_filename = "../UN-english-sample.txt"
+#french_filename = "../UN-french-sample.txt"
+
+google_model = "GoogleNews-vectors-negative300.bin"
+
