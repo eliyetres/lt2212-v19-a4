b90ac62fc0f601811e8970b792458d3e79486976
Azfar Imtiaz <azfar.imtiaz@live.com>
2019-03-15 16:56:04 +0100

    Move function for loading w2v model into vectorization.py, create function to get w2v vectors for entire english vocabulary

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from readfile import readfile
 from split_data import split_data 
-from vectorization import get_vocabulary, generate_one_hot_encoded_vectors
+from vectorization import *
 
 import config
 
 
 if __name__ == '__main__':
+    
+    print("Loading gensim model...")
+    w2v_model = load_gensim_model(config.google_model)
+    
     # getting data from readfile.py
     print("Reading data from source and target files...")
     english_data, french_data = readfile(config.english_filename, config.french_filename)
@@ -18,6 +22,7 @@ if __name__ == '__main__':
     # get vocabulary from training and testing data
     print("Generating vocabulary for source text...")
     eng_vocabulary = get_vocabulary(english_data)
+    print(eng_vocabulary)
     
     print("Generating vocabulary for target text...")
     french_vocabulary = get_vocabulary(french_data)
@@ -28,3 +33,8 @@ if __name__ == '__main__':
     # for index in range(len(one_hot_encoded_vectors_eng)):
     #     print(eng_vocabulary[index])
     #     print(one_hot_encoded_vectors_eng[index])
+
+    # get word2vec vectors for training and testing data
+    w2v_vectors = get_w2v_vectors(w2v_model, eng_vocabulary)
+    print("One hot encoded vector for 'the': {}".format(one_hot_encoded_vectors_eng['the']))
+    print("Word2vec vector for 'the': {}".format(w2v_vectors['the']))
--- vectorization.py
+++ vectorization.py
@@ -1,6 +1,11 @@
 # this file contains helper functions for generating word vectors
 from sklearn.feature_extraction import DictVectorizer
 
+import warnings # Stackoverflow said to do this if you use Windows
+from gensim.models.keyedvectors import KeyedVectors
+
+warnings.filterwarnings(action='ignore', category=UserWarning, module='gensim')
+
 
 def get_vocabulary(ls_text):
     vocabulary = []
@@ -15,5 +20,25 @@ def generate_one_hot_encoded_vectors(ls_words):
     # one_hot_encodings = encoder.fit_transform(ls_words)
     dv = DictVectorizer()
     dvX = dv.fit_transform( [ {'word': a} for a in ls_words ] )
-    one_hot_encodings = dvX.toarray()
+    dvX = dvX.toarray()
+    one_hot_encodings = {}
+    for index in range(len(ls_words)):
+        one_hot_encodings[ls_words[index]] = dvX[index]
     return one_hot_encodings
+
+
+def load_gensim_model(path_to_model):
+    model =  KeyedVectors.load_word2vec_format(path_to_model, binary=True)
+    return model
+
+
+def get_w2v_vectors(w2v_model, ls_words):
+    w2v_vectors = {}
+    for word in ls_words:
+        try:
+            vec = w2v_model.word_vec(word)
+            w2v_vectors[word] = vec
+        # this exception will occur when a word does not exist in the vocabulary of this model
+        except KeyError:
+            continue
+    return w2v_vectors
