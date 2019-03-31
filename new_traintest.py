6abec37cd809ec3af60d3fa42ecdfd16bfca8bb9
Sandra Derbring <sandra.derbring@gmail.com>
2019-03-31 13:27:33 +0200

    added function to create tensors depending on process unit variable

--- new_traintest.py
+++ new_traintest.py
@@ -7,6 +7,18 @@ from trigrams import create_ngram
 from sklearn.metrics import classification_report
 
 
+def make_tensor(vector_list, unit):
+
+    if unit == "gpu":
+        # Using GPU (fast)                                                                                                                                                          
+        X = torch.cuda.FloatTensor(vector_list) # gpu variable must have input type FloatTensor                                                                                           
+    else:
+        # Using CPU (slow)                                                                                                                                                          
+        X = torch.Tensor(vector_list)
+
+    return X
+
+
 def get_predicted_word(predictions, vocab):
     
     #This code is correct, but the problem is that the size of predictions
@@ -51,7 +63,7 @@ def get_top_n_predictions(next_word_pred, n=50):
 #       choose the english word which gives this french word as output -> how to do this?
 #       Take <start> and this english word, feed to trigram model
 
-def test_translation(eng_test, french_test, eng_vocab, french_vocab, w2v_vectors, one_hot_eng, one_hot_french, trigram_model, translation_model):
+def test_translation(eng_test, french_test, eng_vocab, french_vocab, w2v_vectors, one_hot_eng, one_hot_french, trigram_model, translation_model, unit):
 
     # Output variables
     # Measurements should be accuracy, precision, recall and F1_score
@@ -66,8 +78,9 @@ def test_translation(eng_test, french_test, eng_vocab, french_vocab, w2v_vectors
     for word in eng_vocab:
         vect = w2v_vectors[word]
         english_vectors.append(vect)
-    english_vectors_tensor = torch.Tensor(english_vectors)
-
+    #english_vectors_tensor = torch.Tensor(english_vectors)
+    english_vectors_tensor = make_tensor(english_vectors, unit)
+    
     for index in range(len(french_test)):
 
         # for testing only
@@ -114,8 +127,9 @@ def test_translation(eng_test, french_test, eng_vocab, french_vocab, w2v_vectors
             # print("Word {}, {}".format(word_index, french_test[index][word_index]))
 
             bigram = np.hstack((w2v_vectors[first_word], w2v_vectors[second_word]))
-            bigram = torch.Tensor([bigram])
-
+            #bigram = torch.Tensor([bigram])
+            bigram = make_tensor([bigram], unit)
+            
             next_word_pred = trigram_model.predict(bigram)
             
             top_50_prediction_indices = get_top_n_predictions(next_word_pred, n=50)
@@ -128,7 +142,8 @@ def test_translation(eng_test, french_test, eng_vocab, french_vocab, w2v_vectors
 
             top_50_eng_words = [eng_vocab[i] for i in top_50_prediction_indices]
             top_50_eng_vectors = [w2v_vectors[w] for w in top_50_eng_words]
-            top_50_eng_vectors_tensor = torch.Tensor(top_50_eng_vectors)
+            #top_50_eng_vectors_tensor = torch.Tensor(top_50_eng_vectors)
+            top_50_eng_vectors_tensor = make_tensor(top_50_eng_vectors, unit)
 
             translated_predictions = translation_model.predict(top_50_eng_vectors_tensor)
             for i, pred in enumerate(translated_predictions):
