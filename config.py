6e1efe96a1cc748268af6111d654e0be3c83ed8d
Azfar Imtiaz <azfar.imtiaz@live.com>
2019-05-02 11:08:50 +0200

    Remove unused scripts

--- config.py
+++
@@ -1,9 +0,0 @@
-english_filename = "UN-english.txt"
-french_filename = "UN-french.txt"
-
-#english_filename = "../UN-english-sample.txt"
-#french_filename = "../UN-french-sample.txt"
-
-google_model = "GoogleNews-vectors-negative300.bin"
-
-process_unit = "cpu" # gpu (fast) or cpu (slow)
